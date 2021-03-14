f5f1ffdb29248c3f5503d7b4fe878a2884212dc7
AmazingAlek <alek.ntnu@gmail.com>
2021-03-14 22:03:04 +0100

    allowing no step attributes (#8)

---
+++ tests/Dynamocs.DevTools.Tests/Plugins/StepPlugin.cs
@@ -0,0 +1,33 @@
+﻿using Dynamocs.DevTools.Attributes;
+using Dynamocs.DevTools.Extensions;
+using Dynamocs.DevTools.Tests.Models;
+using Microsoft.Xrm.Sdk;
+
+namespace Dynamocs.DevTools.Tests.Plugins
+{
+	[PluginStep("update", "account")]
+	public class StepPlugin : Plugin
+	{
+		public override void Execute(PluginContext<Entity> context)
+		{
+			context.Trace("looool");
+
+			var target = context.Target.ToEntity<Account>();
+			var service = context.OrganizationService;
+
+			target.Name = "foo";
+
+			var lol = new Account
+			{
+				Name = "bar"
+			};
+			var id = service.Create(lol);
+
+			lol = service.RetrieveByAttribute<Account>("name", "bar");
+
+			lol.Id = id;
+			lol.Name = "bar2";
+			service.Update(lol);
+		}
+	}
+}
